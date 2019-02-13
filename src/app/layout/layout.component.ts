8dea9637d633a58ad86b0b1889f8d46f6c91fb76
Kaleb Baldwin <baldwinjkaleb@gmail.com>
2019-02-13 10:48:18 -0500

    create service to track screen size and subscribe/unsubscribe appropriately in LayoutComponent

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ScreenSizeService } from '../services/screen-size.service'
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent implements OnInit, OnDestroy {
+  private _screenSizeSubscription: Subscription;
 
-  constructor() { }
+  constructor(private _screenSize: ScreenSizeService) { }
 
   ngOnInit() {
+    this._screenSizeSubscription = this._screenSize.subscribe(screen => {
+      console.log('size changed', screen);
+    })
   }
 
+  ngOnDestroy() {
+    this._screenSizeSubscription.unsubscribe()
+  }
 }
