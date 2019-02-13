8656804750c2154ac451eac6977a9340a0682c93
Kaleb Baldwin <baldwinjkaleb@gmail.com>
2019-02-12 19:09:51 -0500

    create empty layout and auth components, add corresponding routes, and create an empty auth guard service to enter the layout component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { LayoutComponent } from './layout/layout.component';
+import { AuthGuardService } from './guards/auth-guard.service';
 
-const routes: Routes = [];
+
+const authorizedRoutes: Routes = [
+
+];
+
+const routes: Routes = [
+  { 
+    path: 'auth', 
+    component: AuthComponent 
+  },
+  { 
+    path: '',
+    component: LayoutComponent,
+    canActivate: [ AuthGuardService ],
+    children: authorizedRoutes
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
