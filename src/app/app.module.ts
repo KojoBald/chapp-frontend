da524b4c8fd786560fed904b07a3ffe682458cff
Kaleb Baldwin <baldwinjkaleb@gmail.com>
2019-02-12 19:35:56 -0500

    add Angular Material to project with empty module to import Material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { LayoutComponent } from './layout/layout.component';
@@ -14,7 +16,9 @@ import { LayoutComponent } from './layout/layout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,7 @@
+// add Angular Material component imports here
+import { NgModule } from '@angular/core'
+
+@NgModule({
+    exports: []
+})
+export class MaterialModule { }
