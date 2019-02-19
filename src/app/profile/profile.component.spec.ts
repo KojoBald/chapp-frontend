65d45d653303610575f5b5e3ca6cdfe4e5c4b614
Lars Conard <lars.conard@gmail.com>
2019-02-19 09:41:06 -0500

    Front End Push

--- src/app/profile/profile.component.spec.ts
+++ src/app/profile/profile.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DisplayMessageComponent } from './display-message.component';
+import { ProfileComponent } from './profile.component';
 
-describe('DisplayMessageComponent', () => {
-  let component: DisplayMessageComponent;
-  let fixture: ComponentFixture<DisplayMessageComponent>;
+describe('ProfileComponent', () => {
+  let component: ProfileComponent;
+  let fixture: ComponentFixture<ProfileComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DisplayMessageComponent ]
+      declarations: [ ProfileComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DisplayMessageComponent);
+    fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
