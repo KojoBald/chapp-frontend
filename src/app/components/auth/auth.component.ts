374792439ecc43ce1303f83c5c25db857790226b
MitchellTroyer <mtroyer95@att.net>
2019-02-19 15:51:37 -0500

    sign up

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ApiService } from '../../services/api.service';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-auth',
@@ -10,12 +11,28 @@ export class AuthComponent
 {
   whichForm: string = 'toggle'; 
 
-  constructor(private ApiService: ApiService) {} 
+  constructor(private ApiService: ApiService) {}
 
-  signUp(user) : void
+  ngOnInit(){
+    sessionStorage.setItem("token", "");
+  }
+
+  signUp(first, last, username, email, password, confirmPassword) : void
   {
     event.preventDefault();
+    if(password != confirmPassword){
+      alert("your passwords do not match");
+      return
+    }
+    let user: User = 
+    {
+      first: first,
+      last: last,
+      username: username,
+      email: email,
+      password: password
+    }
     this.ApiService.signUp(user)
-    .subscribe(newUser => newUser);
+    .subscribe(newUser => console.log(newUser));
   }
 }
